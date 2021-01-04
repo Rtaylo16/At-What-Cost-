23ef195aa4e827528b073dfaf57ef3927642fe4b
Rtaylo16 <59778940+Rtaylo16@users.noreply.github.com>
2021-01-04 01:48:16 -0500

    v1

---
+++ at-what-cost/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+
+
+import useStyles from './styles';
+
+const Navbar = ({ totalItems }) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    
+
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography component={Link} to ="/" variant='h6' className={classes.title} color="inherit">
+                        <img src={"https://www.freeiconspng.com/thumbs/vintage-icon-png/camera-vintage-icon-png-5.png"} alt="At What Cost?" height="25px" className={classes.image}/>
+                        At What Cost?
+                    </Typography>
+                    <div className={classes.grow}/>
+                    {location.pathname === '/' && (
+                    <div className={classes.button}>
+                        <IconButton  component={Link} to ="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart/>
+                            </Badge>
+                        </IconButton>
+                    </div>)}
+                </Toolbar>
+
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
