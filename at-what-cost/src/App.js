9b5a791b8b0e650d7842cbae2cb67aeccf1a68fe
Rtaylo16 <59778940+Rtaylo16@users.noreply.github.com>
2021-01-16 19:19:04 -0500

    v5

--- at-what-cost/src/App.js
+++ at-what-cost/src/App.js
@@ -77,7 +77,7 @@ const App = () => {
                 </Route>
 
                 <Route exact path="/checkout">
-                    <Checkout/>
+                    <Checkout cart={ cart }/>
                 </Route>
            
            
--- at-what-cost/src/components/CheckoutForm/PaymentForm.jsx
+++ at-what-cost/src/components/CheckoutForm/PaymentForm.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {Typography, Button, Divider } from "@material-ui/core";
+import {Elements, CardElement, ElementsConsumer}
 
 const PaymentForm = () => {
     return (
